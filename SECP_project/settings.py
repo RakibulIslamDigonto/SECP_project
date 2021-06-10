11b8b4ad96fe7e32d73bd414ed57a0ad1cbfeef5
RakibulIslamDigonto <rakibdigonto11@gmail.com>
2021-06-11 00:20:24 +0600

    debug updated

--- SECP_project/settings.py
+++ SECP_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-t5brbpbcm_j07m^xp*g9i4fd6hwut9y$89$5!zdyrfva(ivr7h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'codeprofile.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- SECP_project/urls.py
+++ SECP_project/urls.py
@@ -25,6 +25,6 @@ urlpatterns.append(url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
 urlpatterns += staticfiles_urlpatterns()
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#     document_root=settings.MEDIA_ROOT)
